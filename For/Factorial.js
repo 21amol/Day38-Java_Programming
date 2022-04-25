cd1a5dc75b524d8c788891da885a792469f75376
Amol Ghotale <ghotaleamol@gmail.com>
2022-04-25 21:44:04 +0530

    ADDED: Factorial Program

---
+++ For/Factorial.js
@@ -0,0 +1,9 @@
+// Finding factorials....
+
+var num = Math.floor(Math.random() * 10);
+
+var a = 1;
+for(var i = 1; i <= num; i++) {
+a = a * i;
+}
+console.log("The factorial value of " +num + " is: " +a);
